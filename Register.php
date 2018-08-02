a2369a0c6f10842a7111cfdb3521596054e0b3e7
paolaelenabernardelli <32967788+paolaelenabernardelli@users.noreply.github.com>
2018-08-02 23:52:47 +0200

    Add files via upload

---
+++ Register.php
@@ -0,0 +1,22 @@
+
+
+<?php 
+
+// Parameters to modify $WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php'; 
+$BOT_TOKEN = '{TOKEN}';
+$API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
+$method = 'setWebhook';
+$parameters = array('url' => $WEBHOOK_URL);
+$url = $API_URL . $method. '?' . http_build_query($parameters);
+$handle = curl_init($url); 
+curl_setopt($handle, CURLOPT_RETURNTRANSFER, true); 
+curl_setopt($handle, CURLOPT_CONNECTTIMEOUT, 5);
+curl_setopt($handle, CURLOPT_TIMEOUT, 60); 
+$result = curl_exec($handle); 
+print_r($result);
+
+
+
+
+?>
+
---
+++ index.php
@@ -0,0 +1,7 @@
+
+
+<?php echo "It’s all right!";
+
+
+?>
+
