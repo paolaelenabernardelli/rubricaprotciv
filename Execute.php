904b513ce29e06de0214b6932f7381e1b3f8a5b6
paolaelenabernardelli <32967788+paolaelenabernardelli@users.noreply.github.com>
2018-08-08 20:04:35 +0200

    Update Execute.php

--- Execute.php
+++ Execute.php
@@ -5,9 +5,9 @@ $content = file_get_contents("php://input");
 $update = json_decode($content, true); 
 
 if(!$update) { 
-  echo "il boot non funziona";
-//  exit; 
+  exit; 
 } 
+// assegno alle seguenti variabili il contenuto ricevuto da Telegram
 $message = isset($update['message']) ? $update['message'] : ""; 
 $messageId = isset($message['message_id']) ? $message['message_id'] : ""; 
 $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : ""; 
@@ -16,11 +16,25 @@ $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name']
 $username = isset($message['chat']['username']) ? $message['chat']['username'] : ""; 
 $date = isset($message['date']) ? $message['date'] : ""; 
 $text = isset($message['text']) ? $message['text'] : ""; 
-$text = trim($text); $text = strtolower($text); header("Content-Type: application/json"); 
-$parameters = array('chat_id' => $chatId, "text" => $text); 
-$parameters["method"] = "sendMessage"; echo json_encode($parameters);
-if ($text='ciao') {$response = "Ciao $firstname $lastname il tuo username è $username";}
-else {$response = "Ciao $firstname $lastname il tuo username è $username";}
+
+// pulisco il messaggio ricevuto togliendo eventuali spazi prima e dopo il testo
+$text = trim($text);
+// converto tutti i caratteri alfanumerici del messaggio in minuscolo
+$text = strtolower($text);
+// mi preparo a restitutire al chiamante la mia risposta che è un oggetto JSON
+// imposto l'header della risposta
+header("Content-Type: application/json");
+// la mia risposta è un array JSON composto da chat_id, text, method
+// chat_id mi consente di rispondere allo specifico utente che ha scritto al bot
+// text è il testo della risposta
+$parameters = array('chat_id' => $chatId, "text" => $text);
+// method è il metodo per l'invio di un messaggio (cfr. API di Telegram)
+$parameters["method"] = "sendMessage";
+// converto e stampo l'array JSON sulla response
+echo json_encode($parameters);
+
+//if ($text='ciao') {$response = "Ciao $firstname $lastname il tuo username è $username";}
+//else {$response = "Ciao $firstname $lastname il tuo username è $username";}
 //if(strpos($text, "/start") === 0 || $text=="ciao")
 //{
 //$tastiera=0;
