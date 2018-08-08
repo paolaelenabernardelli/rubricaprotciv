77014af238d89f926c09bff298ad779ddbd39340
paolaelenabernardelli <32967788+paolaelenabernardelli@users.noreply.github.com>
2018-08-08 23:25:20 +0200

    Update Execute.php

--- Execute.php
+++ Execute.php
@@ -8,33 +8,43 @@ if(!$update) {
   exit; 
 } 
 // assegno alle seguenti variabili il contenuto ricevuto da Telegram
-$message = isset($update['message']) ? $update['message'] : ""; 
-$messageId = isset($message['message_id']) ? $message['message_id'] : ""; 
-$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : ""; 
-$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
-$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : ""; 
-$username = isset($message['chat']['username']) ? $message['chat']['username'] : ""; 
-$date = isset($message['date']) ? $message['date'] : ""; 
-$text = isset($message['text']) ? $message['text'] : ""; 
+// $message = isset($update['message']) ? $update['message'] : ""; 
+// $messageId = isset($message['message_id']) ? $message['message_id'] : ""; 
+// $chatId = isset($message['chat']['id']) ? $message['chat']['id'] : ""; 
+// $firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
+// $lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : ""; 
+// $username = isset($message['chat']['username']) ? $message['chat']['username'] : ""; // 
+// $date = isset($message['date']) ? $message['date'] : ""; 
+// $text = isset($message['text']) ? $message['text'] : ""; 
+
+$message=$update['message']['text'];
+$chatId=$update['message']['text']['id'];
+
+function sendMessage ($chatId, $text) {
+  global $api;
+  $api='bot542609682:AAEefoEGUUriQtztOG-Yf5LSetKv1EmpxyQ';
+  $url='https://api.telegram.org/$api/sendMessage?chatid=$chatid$text='.urlencode($text); 
+  $get = file_get_contents($url);
+  }
 
 // pulisco il messaggio ricevuto togliendo eventuali spazi prima e dopo il testo
-$text = trim($text);
+//$text = trim($text);
 // converto tutti i caratteri alfanumerici del messaggio in minuscolo
-$text = strtolower($text);
+//$text = strtolower($text);
 // mi preparo a restitutire al chiamante la mia risposta che è un oggetto JSON
 // imposto l'header della risposta
-header("Content-Type: application/json");
+/header("Content-Type: application/json");
 // la mia risposta è un array JSON composto da chat_id, text, method
 // chat_id mi consente di rispondere allo specifico utente che ha scritto al bot
 // text è il testo della risposta
-$parameters = array('chat_id' => $chatId, "text" => $text);
+//$parameters = array('chat_id' => $chatId, "text" => $text);
 // method è il metodo per l'invio di un messaggio (cfr. API di Telegram)
-$parameters["method"] = "sendMessage";
+//$parameters["method"] = "sendMessage";
 // imposto la inline keyboard
-$keyboard = ['inline_keyboard' => [[['text' =>  'myText', 'callback_data' => 'myCallbackText']]]];
-$parameters["reply_markup"] = json_encode($keyboard, true);
+//$keyboard = ['inline_keyboard' => [[['text' =>  'myText', 'callback_data' => 'myCallbackText']]]];
+//$parameters["reply_markup"] = json_encode($keyboard, true);
 // converto e stampo l'array JSON sulla response
-echo json_encode($parameters);
+//echo json_encode($parameters);
 
 if ($text='/ricerca') {$response = "Ciao $firstname $lastname il tuo username è $username";
                   echo $response;}
