cf8f471eb1279b9076179c593a4a7272e65859f8
paolaelenabernardelli <32967788+paolaelenabernardelli@users.noreply.github.com>
2018-12-11 19:44:41 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -1,23 +1,29 @@
 
 
 <?php echo "It’s all right!";
-// recupero il contenuto inviato da Telegram
-$content=file_get_contents("php://input");
-// converto il contenuto da JSON ad array PHP
-$update=json_decode($content, true); 
-if(!$update) { 
-  exit; 
-} 
-$website="https://api.telegram.org/";
-$text=$update['message']['text'];
-$chatid=$update['message']['from']['id'];
-$agg=json_encode($update, JSON_PRETTY_PRINT);
+$content = file_get_contents("php://input");
+$update = json_decode($content, true);
 
-function sendMessage($chatId, $text) {
-  $api='bot542609682:AAEefoEGUUriQtztOG-Yf5LSetKv1EmpxyQ';
-  $url=$GLOBALS[$website].$api."/sendMessage?chatid=$chatid&$text=".urlencode($text); 
-  $get = file_get_contents($url);
-  }
-sendMessage($chatid,"Ciao");
+if(!$update)
+{
+  exit;
+}
+
+$message = isset($update['message']) ? $update['message'] : "";
+$messageId = isset($message['message_id']) ? $message['message_id'] : "";
+$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
+$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
+$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
+$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
+$date = isset($message['date']) ? $message['date'] : "";
+$text = isset($message['text']) ? $message['text'] : "";
+
+$text = trim($text);
+$text = strtolower($text);
+
+header("Content-Type: application/json");
+$parameters = array('chat_id' => $chatId, "text" => $text);
+$parameters["method"] = "sendMessage";
+echo json_encode($parameters);
 ?>
 
