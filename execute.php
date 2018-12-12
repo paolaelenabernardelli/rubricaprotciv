b5bd6ddad3b0902ea4cd355d147e913ec9e3be8f
paolaelenabernardelli <32967788+paolaelenabernardelli@users.noreply.github.com>
2018-12-12 04:22:36 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -9,21 +9,14 @@ if(!$update)
   exit;
 }
 
-$message = isset($update['message']) ? $update['message'] : "";
-$messageId = isset($message['message_id']) ? $message['message_id'] : "";
-$chatId = isset($message['chat']['id']) ? $message['chat']['id'] : "";
-$firstname = isset($message['chat']['first_name']) ? $message['chat']['first_name'] : "";
-$lastname = isset($message['chat']['last_name']) ? $message['chat']['last_name'] : "";
-$username = isset($message['chat']['username']) ? $message['chat']['username'] : "";
-$date = isset($message['date']) ? $message['date'] : "";
-$text = isset($message['text']) ? $message['text'] : "";
+$apiToken = "542609682:AAEefoEGUUriQtztOG-Yf5LSetKv1EmpxyQ";
 
-$text = trim($text);
-$text = strtolower($text);
+$data = [
+    'chat_id' => '@my_channel_name',
+    'text' => 'Hello world!'
+];
 
-header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $text);
-$parameters["method"] = "sendMessage";
-echo json_encode($parameters);
+$response = file_get_contents("https://api.telegram.org/bot$apiToken/sendMessage?".http_build_query($data) );
+// Do what you want with result
 ?>
 
